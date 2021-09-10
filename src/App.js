f308e40584e05fce73a33d46334c5e9d781593d6
TrongHuu-Nguyen <beenguyentrong@gmail.com>
2021-09-10 21:29:58 +0700

    login redirect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ProductPage from "./pages/productPage/productPage/productpage"
 import ProductDetail from "./pages/productDetail/productDetailPage/productDetail"
 import HomePage from "./pages/homePage/homepage";
 import LoginPage from "./pages/Login/loginPage"
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route,Redirect } from "react-router-dom";
 import 'antd/dist/antd.css'
 import './styles/App.css'
 
@@ -13,7 +13,11 @@ function App() {
         <Route path="/" exact ><HomePage /></Route>
         <Route path="/product"><ProductPage /></Route>
         <Route path="/product-detail"> <ProductDetail /></Route>
-        <Route path="/login"> <LoginPage /></Route>
+        <Route path="/login" render={()=>{
+          return !localStorage.getItem("access")?<LoginPage />:<Redirect to="/"/>
+        }}>
+          
+        </Route>
       </Switch>
     </div>
   )
--- src/pages/Login/loginPage.js
+++ src/pages/Login/loginPage.js
@@ -3,20 +3,20 @@ import loginImage from '../../asset/pngegg.png'
 import Footer from '../../layout/footer/footer'
 import React from 'react'
 import { ShopOutlined } from '@ant-design/icons'
-import {Link} from 'react-router-dom'
+import { Link,useHistory } from 'react-router-dom'
 import _ from 'lodash'
 
 const LoginPage = () => {
-    let listUser=[];
-    const users=JSON.parse(localStorage.getItem("userAccount"))||[];
-    
+    let listUser = [];
+    const users = JSON.parse(localStorage.getItem("userAccount")) || [];
+    let history=useHistory();
     React.useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
     React.useEffect(() => {
-        listUser=[...users];
+        listUser = [...users];
     }, [users]);
-   
+
     //input ref
     let userIdRef = React.useRef(null);
     let userPassRef = React.useRef(null);
@@ -32,68 +32,70 @@ const LoginPage = () => {
     // constants
     const idPattern = /^[a-zA-Z0-9_]{5,16}$/;
     const passwordPattern = /^[a-zA-Z0-9]{8,20}$/;
-    const idErrorMess="5-16 characters '_' and numbers, no special characters";
-    const idDuplicateErrorMess="Account already exists";
-    const passErrorMess="8-20 characters and numbers, no special characters";
-    const rewritepassErrorMess="re-enter password does not match";
-    const signInErrorMess="Incorrect account or password";
+    const idErrorMess = "5-16 characters '_' and numbers, no special characters";
+    const idDuplicateErrorMess = "Account already exists";
+    const passErrorMess = "8-20 characters and numbers, no special characters";
+    const rewritepassErrorMess = "re-enter password does not match";
+    const signInErrorMess = "Incorrect account or password";
+    const NotExistErrorMess = "Account does not exist";
 
-    const clearAll=()=>{
-        userIdErrorRef.current.innerHTML="";
-        userPassErrorRef.current.innerHTML="";
-        registerIdErrorRef.current.innerHTML="";
-        registerPassErrorRef.current.innerHTML="";
-        reWritePassErrorRef.current.innerHTML="";
-        userIdRef.current.value="";
-        userPassRef.current.value="";
-        registerIdRef.current.value="";
-        registerPassRef.current.value="";
-        reWritePassRef.current.value="";
+
+    const clearAll = () => {
+        userIdErrorRef.current.innerHTML = "";
+        userPassErrorRef.current.innerHTML = "";
+        registerIdErrorRef.current.innerHTML = "";
+        registerPassErrorRef.current.innerHTML = "";
+        reWritePassErrorRef.current.innerHTML = "";
+        userIdRef.current.value = "";
+        userPassRef.current.value = "";
+        registerIdRef.current.value = "";
+        registerPassRef.current.value = "";
+        reWritePassRef.current.value = "";
     };
-   
+
 
     const UserSignUp = () => {
-        let isValid=true;
-        let registerId=registerIdRef.current.value;
-        let registerPass=registerPassRef.current.value;
-        let reWritePass=reWritePassRef.current.value;
-        const validate=(item,validate,error,mess)=>{
-            if(item===""){
-                error.current.innerHTML="*Required";
-                return isValid=false;
+        let isValid = true;
+        let registerId = registerIdRef.current.value;
+        let registerPass = registerPassRef.current.value;
+        let reWritePass = reWritePassRef.current.value;
+        const validate = (item, validate, error, mess) => {
+            if (item === "") {
+                error.current.innerHTML = "*Required";
+                return isValid = false;
             }
-            if(!validate.test( item)){
-                error.current.innerHTML=mess;
-                return isValid=false;
+            if (!validate.test(item)) {
+                error.current.innerHTML = mess;
+                return isValid = false;
             }
-            if(validate.test( item)){
-                error.current.innerHTML="";
+            if (validate.test(item)) {
+                error.current.innerHTML = "";
             }
         }
-        validate(registerId,idPattern,registerIdErrorRef,idErrorMess);
-        validate(registerPass,passwordPattern,registerPassErrorRef,passErrorMess);
-        validate(reWritePass,passwordPattern,reWritePassErrorRef,passErrorMess);
+        validate(registerId, idPattern, registerIdErrorRef, idErrorMess);
+        validate(registerPass, passwordPattern, registerPassErrorRef, passErrorMess);
+        validate(reWritePass, passwordPattern, reWritePassErrorRef, passErrorMess);
 
-        if(_.findIndex(users,["userId",registerId])>=0){
-            registerIdErrorRef.current.innerHTML=idDuplicateErrorMess;
-            return isValid=false;
+        if (_.findIndex(users, ["userId", registerId]) >= 0) {
+            registerIdErrorRef.current.innerHTML = idDuplicateErrorMess;
+            return isValid = false;
         }
-        if(reWritePass!==registerPass){
-            reWritePassErrorRef.current.innerHTML=rewritepassErrorMess;
-            return isValid=false;
+        if (reWritePass !== registerPass) {
+            reWritePassErrorRef.current.innerHTML = rewritepassErrorMess;
+            return isValid = false;
         }
-        let account={
-            userId:registerId,
-            userPass:registerPass,
-            id:Math.random().toString(32).slice(2),
-            userComment:[],
-            userWish:[],
-            userCart:[]
+        let account = {
+            userId: registerId,
+            userPass: registerPass,
+            id: Math.random().toString(32).slice(2),
+            userComment: [],
+            userWish: [],
+            userCart: []
         }
         listUser.push(account);
 
-        if(isValid){
-            localStorage.setItem("userAccount",JSON.stringify(listUser))
+        if (isValid) {
+            localStorage.setItem("userAccount", JSON.stringify(listUser))
             clearAll();
             setIsShow(false);
             alert("Ban Da Dang ky thanh cong!");
@@ -102,40 +104,44 @@ const LoginPage = () => {
 
 
     const UserSignIn = () => {
-        let isValid=true;
-        let userId=userIdRef.current.value;
-        let userPass=userPassRef.current.value;
-        const validate=(item,validate,error,mess)=>{
-            if(item===""){
-                error.current.innerHTML="*Required";
-                return isValid=false;
+        let isValid = true;
+        let userId = userIdRef.current.value;
+        let userPass = userPassRef.current.value;
+        const validate = (item, validate, error, mess) => {
+            if (item === "") {
+                error.current.innerHTML = "*Required";
+                return isValid = false;
             }
-            if(!validate.test( item)){
-                error.current.innerHTML=mess;
-                return isValid=false;
+            if (!validate.test(item)) {
+                error.current.innerHTML = mess;
+                return isValid = false;
             }
-            if(validate.test( item)){
-                error.current.innerHTML="";
+            if (validate.test(item)) {
+                error.current.innerHTML = "";
             }
         }
-        validate(userId,idPattern,userIdErrorRef,idErrorMess);
-        validate(userPass,passwordPattern,userPassErrorRef,passErrorMess);
-        const currentUser=_.find(users,{"userId":userId,"userPass":userPass});
-        
-        if(isValid&&(currentUser===undefined)){
-            userPassErrorRef.current.innerHTML=signInErrorMess;
-            return isValid=false;
+        validate(userId, idPattern, userIdErrorRef, idErrorMess);
+        validate(userPass, passwordPattern, userPassErrorRef, passErrorMess);
+        const currentUser = _.find(users, { "userId": userId, "userPass": userPass });
+
+        if (isValid && _.findIndex(users, ["userId", userId]) < 0) {
+            userIdErrorRef.current.innerHTML = NotExistErrorMess;
+            return isValid = false;
         }
-       
-        
-        if(isValid){
-            localStorage.setItem("currentUser",JSON.stringify(currentUser));
+
+        if (isValid && (currentUser === undefined)) {
+            userPassErrorRef.current.innerHTML = signInErrorMess;
+            return isValid = false;
+        }
+
+        if (isValid) {
+            localStorage.setItem("currentUser", JSON.stringify(currentUser));
+            localStorage.setItem("access", JSON.stringify(true));
             clearAll();
             alert("Dang nhap thanh cong!");
+            history.replace("/");
         }
     };
-    
-
 
     const [isShow, setIsShow] = React.useState(false);
 
@@ -148,11 +154,11 @@ const LoginPage = () => {
         clearAll();
         setIsShow(false);
     };
-    
+
     return (
         <div className="LoginPage">
             <div className="LoginBox">
-               
+
                 <div className="LoginImage">
                     <img src={loginImage} alt="login" />
                 </div>
@@ -163,21 +169,21 @@ const LoginPage = () => {
                     </p>
                     <div className="InputID">
 
-                        <label for="UserID">User ID</label><br/><br />
+                        <label for="UserID">User ID</label><br />
                         <input ref={userIdRef} type="text" placeholder="Your ID" id="UserID" className="LoginInput" required></input>
-                        <span ref={userIdErrorRef}></span><br/>
+                        <br /><span ref={userIdErrorRef}></span><br />
 
                     </div><br />
                     <div className="InputPassword">
 
                         <label for="UserPassword">User Password</label><br />
-                        <input ref={userPassRef} type="text" placeholder="Your Password" id="UserPassword" className="LoginInput" required></input>
-                        <br/><span ref={userPassErrorRef}></span><br/>
+                        <input ref={userPassRef} type="password" placeholder="Your Password" id="UserPassword" className="LoginInput" required></input>
+                        <br /><span ref={userPassErrorRef}></span><br />
 
                     </div><br />
                     <div className="InputSubmit">
                         <div className="SubmitBtn" onClick={UserSignIn}><p>LOG IN</p></div>
-                        <div className="HomeBtn"><Link to="/"><p><ShopOutlined style={{fontSize: "16px"}}/>&nbsp;HOME PAGE</p></Link></div>
+                        <div className="HomeBtn"><Link to="/"><p><ShopOutlined style={{ fontSize: "16px" }} />&nbsp;HOME PAGE</p></Link></div>
                     </div>
                     {/* <Divider>Or</Divider> */}
                 </div>
@@ -192,22 +198,22 @@ const LoginPage = () => {
 
                                 <label for="RegisterUserID">User ID</label><br />
                                 <input ref={registerIdRef} type="text" placeholder="Enter Your ID" id="RegisterUserID" className="LoginInput" required></input>
-                                <br/><span ref={registerIdErrorRef}></span><br/>
+                                <br /><span ref={registerIdErrorRef}></span><br />
 
                             </div>
-                            
+
                             <div className="InputPassword">
 
                                 <label for="RegisterUserPassword">User Password</label><br />
                                 <input ref={registerPassRef} type="password" placeholder="Enter Your Password" id="RegisterUserPassword" className="LoginInput" required></input>
-                                <br/><span ref={registerPassErrorRef}></span><br/>
+                                <br /><span ref={registerPassErrorRef}></span><br />
 
                             </div>
                             <div className="InputPassword">
 
                                 <label for="RegisterUserPasswordRewrite">Rewrite Your Password</label><br />
                                 <input ref={reWritePassRef} type="password" placeholder="Rewrite Your Password" id="RegisterUserPasswordRewrite" className="LoginInput" required></input>
-                                <br/><span ref={reWritePassErrorRef}></span><br/>
+                                <br /><span ref={reWritePassErrorRef}></span><br />
 
                             </div><br />
                             <div className="InputSubmit">
